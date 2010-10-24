20a5c5eaee700c29ce0a59da2317c4c6e68a7bde
Alex Iceman <sasharef@gmail.com>
2010-10-24 23:07:08 +0400

    first commit

---
+++ HACKING/template.c
@@ -0,0 +1,31 @@
+/**
+ * @file   <filename>
+ * @brief  Brief description.
+ * Detailed description starts here.
+ *
+ * @internal
+ * Copyright 20xx James Hacker <james@hacker.com>
+ * Copyright 20yy, 20zz, 20tt John Doe <john@example.com>
+ *
+ * This file is part of Yaposhiga.
+ *
+ * Yaposhiga is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Yaposhiga is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * See the full text of the GNU General Public License in the COPYING
+ * file in source code directory.
+ *
+ * If you are unable to read that file, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// your code goes here
+
+// vim:sw=4:ts=4:et
---
+++ src/crypto.cpp
@@ -0,0 +1,7 @@
+/*
+ * crypto.cpp
+ *
+ *  Created on: 24.10.2010
+ *      Author: alex
+ */
+
